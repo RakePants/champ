e13ace38ab466a8ce51c66f2aa8cb5bc7929aa9e
Artyom Eryomkin <eryomkin.artyom2016@gmail.com>
2024-03-28 13:42:06 +0500

    Move processing microservice to another branch

--- processing_service/src/config.py
+++
@@ -1,9 +0,0 @@
-import logging
-from pathlib import Path
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
-BASE_DIR = Path(__file__).parent
--- processing_service/src/processing/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import BinaryIO
-
-
-def process_image(image: BinaryIO) -> BinaryIO:
-    return image
-
-
-def process_speech_to_text(audio: BinaryIO) -> BinaryIO:
-    return audio
