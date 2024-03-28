373f7278277f34ec3cd42826d3cab7f7604558c2
Artyom Eryomkin <eryomkin.artyom2016@gmail.com>
2024-03-28 13:16:19 +0500

    Add CORS to processing_service

--- processing_service/src/main.py
+++ processing_service/src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.processing.router import processing_router
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
