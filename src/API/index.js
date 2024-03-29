f54ecbc3737db906432bdd7ec39866afa0d18686
Sh6abrA <47980718+Sh6abrA@users.noreply.github.com>
2024-03-29 14:29:08 +0500

    Change Url to localhost

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://2.58.70.16:7778";
+const BASE_URL = "localhost:7778";
 const OLD_URL = 'http://212.60.20.177:7778';
 const axiosInstance = axios.create({
-    baseURL: OLD_URL
+    baseURL: BASE_URL
 })
 
 export default axiosInstance
